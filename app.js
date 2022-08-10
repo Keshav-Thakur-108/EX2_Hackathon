65e574ad84f331a2bb93d0ee062d3f3fe7a90265
Keshav Thakur <keshavthakur.001@gmail.com>
2022-08-10 12:35:01 +0530

    Add University Data in public and Add '/' route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.get('/', (req, res) => {
+    res.send("Homepage")
+})
+
 app.listen(3000, function (req, res) {
   console.log("The server is started");
 });
