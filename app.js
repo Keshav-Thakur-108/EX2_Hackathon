2a654ae65f1832773b548d49c518d3ab8836be56
Keshav Thakur <keshavthakur.001@gmail.com>
2022-08-10 16:52:32 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,12 +1,87 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const csv=require('csvtojson');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.send("Homepage")
+let dataArray;
+
+
+const csvFilePath='./public/data/university_data.csv';
+
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    dataArray = jsonObj
 })
+let fees = undefined;
+let rat = undefined ;
+let loc = undefined ;
+app.post('/',(req,res)=>{
+    fee=req.query.fees;
+    rat=req.query.rating;
+    loc=req.query.location;
+
+
+    let result=[];
+    let siz=dataArray.length;
+    rat=rat.toLowerCase();
+
+    for(let i=0;i<siz;i++)
+     {
+        
+        if(fee===undefined)
+        {
+            if(dataArray[i].rank <= rat && dataArray[i].state.toLowerCase() === loc)
+            {
+               
+               result.push(dataArray[i]);
+            }
+        }
+        else if(loc===undefined)
+        {
+            if(dataArray[i].fees <= fee && dataArray[i].rank <= rat )
+            {
+               
+               result.push(dataArray[i]);
+            }
+        }
+        else if(!rat)
+        {
+            if(dataArray[i].fees <= fee &&  dataArray[i].state.toLowerCase() === loc)
+            {
+               result.push(dataArray[i]);
+            }
+        }
+        else
+        {
+            if(dataArray[i].fees <= fee && dataArray[i].rank <= rat && dataArray[i].state.toLowerCase() === loc)
+            {
+               
+               result.push(dataArray[i]);
+            }
+        }
+        
+        
+     }  
+    res.send(result);
+
+});
+
+app.get('/', (req, res) => {
+    res.render("index");
+
+})
+
+app.get('/data', (req, res) => {
+    res.send(dataArray)
+})
+
 
 app.listen(3000, function (req, res) {
   console.log("The server is started");
